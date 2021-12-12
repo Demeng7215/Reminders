b4c37d9d029c6f0c948718bcee7a15038da63f12
Demeng7215 <demengc6@gmail.com>
2021-12-12 14:58:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Reminders
+ICS3U7 Final Project - Simple utility application for keeping track of your tasks.
---
+++ src/me/shreymeng/reminders/Main.java
@@ -0,0 +1,8 @@
+package me.shreymeng.reminders;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
