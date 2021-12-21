65bcc4e03ceea789f0e4b0aee2f5a18e98c51556
Demeng7215 <demengc6@gmail.com>
2021-12-20 21:31:15 -0500

    Add sorting by 2 different methods

--- src/me/shreymeng/reminders/manager/RemindersManager.java
+++ src/me/shreymeng/reminders/manager/RemindersManager.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import me.shreymeng.reminders.model.Label;
 import me.shreymeng.reminders.model.Reminder;
+import me.shreymeng.reminders.model.SortBy;
 
 public class RemindersManager {
 
-  //TODO Load this data from a database or flat file on startup, and save after modifying.
+  //TODO Implement GSON file saving/loading.
   private static final List<Reminder> reminders = new ArrayList<>();
 
   public static void addReminder(Reminder reminder) {
@@ -18,97 +19,14 @@ public class RemindersManager {
   public static void removeReminder(Reminder reminder) {
     reminders.remove(reminder);
   }
-  
-  //TODO TEST : Sort by due date and priority.
-  public static List<Reminder> getReminders() {
-	List<Long> very_high_dueDates = new ArrayList<>();
-	List<Long> high_dueDates = new ArrayList<>();
-	List<Long> normal_dueDates = new ArrayList<>();
-	List<Long> low_dueDates = new ArrayList<>();
-	List<Reminder> sortedReminders = new ArrayList<>();
 
-	for (Reminder reminder : reminders) {
-		if (reminder.getPriority() == Priority.VERY_HIGH) {
-			very_high_dueDates.add(reminder.getDueDate());
-
-		} else if (reminder.getPriority() == Priority.HIGH) {
-			high_dueDates.add(reminder.getDueDate());
-
-		} else if (reminder.getPriority() == Priority.NORMAL) {
-			normal_dueDates.add(reminder.getDueDate());
-
-		} else if (reminder.getPriority() == Priority.LOW) {
-			low_dueDates.add(reminder.getDueDate());
-		}
-	}
-
-	selectionSort(very_high_dueDates);
-	for (int i = 0; i < very_high_dueDates.size(); i++) {
-		for (int j = 0; j < reminders.size(); j++) {
-			if (reminders.get(j).getPriority() == Priority.VERY_HIGH && reminders.get(j).getDueDate() == very_high_dueDates.get(i)) {
-				sortedReminders.add(reminders.get(j));
-			}
-		}
-	}
-
-	selectionSort(high_dueDates);
-	for (int i = 0; i < high_dueDates.size(); i++) {
-		for (int j = 0; j < reminders.size(); j++) {
-			if (reminders.get(j).getPriority() == Priority.HIGH && reminders.get(j).getDueDate() == high_dueDates.get(i)) {
-				sortedReminders.add(reminders.get(j));
-			}
-		}
-	}
-
-	selectionSort(normal_dueDates);
-	for (int i = 0; i < normal_dueDates.size(); i++) {
-		for (int j = 0; j < reminders.size(); j++) {
-			if (reminders.get(j).getPriority() == Priority.NORMAL && reminders.get(j).getDueDate() == normal_dueDates.get(i)) {
-				sortedReminders.add(reminders.get(j));
-			}
-		}
-	}
-
-	selectionSort(low_dueDates);
-	for (int i = 0; i < low_dueDates.size(); i++) {
-		for (int j = 0; j < reminders.size(); j++) {
-			if (reminders.get(j).getPriority() == Priority.LOW && reminders.get(j).getDueDate() == low_dueDates.get(i)) {
-				sortedReminders.add(reminders.get(j));
-			}
-		}
-	}
-
-	for(int i = 0; i < sortedReminders.size(); i++) {
-		reminders.set(i, sortedReminders.get(i));
-	}
-
-	return reminders;
-	}
-
-  //TODO Sort by due date and priority.
-  public static List<Reminder> getRemindersByLabel(Label label) {
-    return reminders.stream()
-        .filter(reminder -> reminder.getLabels().contains(label))
-        .collect(Collectors.toList());
+  public static List<Reminder> getReminders(SortBy sort) {
+    return sort.sort(reminders);
   }
-  
-  public static void selectionSort(List<Long> arr) {
-	long start = 0L;
-	long end = 0L;
 
-	for (int i = 0; i < arr.size(); i++) {
-		long min = arr.get(i);
-		int minInd = i;
-		for (int j = i+1; j < arr.size(); j++) {
-			if (arr.get(j) < min) {
-				min = arr.get(j);
-				minInd = j;
-			}
-		}
-		// swapping
-		long swap = arr.get(i);
-		arr.set(i, min);
-		arr.set(minInd, swap);
-	}
-}
+  public static List<Reminder> getRemindersByLabel(Label label, SortBy sort) {
+    return sort.sort(reminders.stream()
+        .filter(reminder -> reminder.getLabels().contains(label))
+        .collect(Collectors.toList()));
+  }
 }
---
+++ src/me/shreymeng/reminders/model/SortBy.java
@@ -0,0 +1,58 @@
+package me.shreymeng.reminders.model;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public enum SortBy {
+  DUE_DATE(new DueDateSort()), PRIORITY(new PrioritySort());
+
+  private final SortingMethod method;
+
+  SortBy(SortingMethod method) {
+    this.method = method;
+  }
+
+  public List<Reminder> sort(List<Reminder> unsorted) {
+    return method.sort(unsorted);
+  }
+
+  private interface SortingMethod {
+
+    List<Reminder> sort(List<Reminder> unsorted);
+  }
+
+  private static class DueDateSort implements SortingMethod {
+
+    @Override
+    public List<Reminder> sort(List<Reminder> unsorted) {
+      final List<Reminder> reminders = new ArrayList<>(unsorted);
+      reminders.sort((o1, o2) -> (int) (o1.getDueDate() - o2.getDueDate()));
+      return reminders;
+    }
+  }
+
+  private static class PrioritySort implements SortingMethod {
+
+    @Override
+    public List<Reminder> sort(List<Reminder> unsorted) {
+
+      final List<Reminder> reminders = new ArrayList<>();
+      final Map<Priority, List<Reminder>> priorityMap = new EnumMap<>(Priority.class);
+
+      for (Priority priority : Priority.values()) {
+        priorityMap.put(priority, unsorted.stream()
+            .filter(reminder -> reminder.getPriority() == priority)
+            .collect(Collectors.toList()));
+      }
+
+      for (List<Reminder> list : priorityMap.values()) {
+        reminders.addAll(DUE_DATE.sort(list));
+      }
+
+      return reminders;
+    }
+  }
+}
