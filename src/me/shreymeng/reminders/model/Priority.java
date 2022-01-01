c4673835672b6182287946f60bca9b72d32bf3c9
Demeng7215 <demengc6@gmail.com>
2022-01-01 15:22:51 -0500

    Add display names and colors to Priority

--- src/me/shreymeng/reminders/model/Priority.java
+++ src/me/shreymeng/reminders/model/Priority.java
@@ -1,8 +1,30 @@
 package me.shreymeng.reminders.model;
 
+import java.awt.Color;
+
 /**
  * Reminder priorities- must be listed from lowest to highest.
  */
 public enum Priority {
-  LOW, NORMAL, HIGH, VERY_HIGH
+  LOW("• Low Priority", Color.decode("#006e1a")),
+  NORMAL("• Normal Priority", Color.decode("#0050e6")),
+  HIGH("• High Priority", Color.decode("#ff2e2e")),
+  VERY_HIGH("• Very High Priority", Color.decode("#870000"));
+
+  private final String name;
+  private final Color color;
+
+  Priority(String name, Color color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  @Override
+  public String toString() {
+    return name;
+  }
+
+  public Color getColor() {
+    return color;
+  }
 }
